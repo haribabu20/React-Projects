bcae82c43bdfe9243f15822850ec514da050e67f
haribabu20 <hariselva2000@gmail.com>
2026-06-05 15:37:23 +0530

    feat: Password Encrypted

--- 03-Dice-game/backend/controllers/authController.js
+++ 03-Dice-game/backend/controllers/authController.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcryptjs");
 const User = require("../models/User");
 
 // This function handles registeration request
@@ -6,22 +7,28 @@ const registerUser = async (req, res) => {
     const { name, email, password } = req.body;
 
     const existingUser = await User.findOne({ email });
-
+    
     if (existingUser) {
       return res.status(400).json({
         message: "User already exists",
       });
     }
 
+    const hashedPassword = await bcrypt.hash(password, 10); // shldn't hash the password before checking if the user alread exist.
+
     const user = await User.create({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
 
     res.status(201).json({
       message: "User registered successfully",
-      user,
+      user: {
+        _id: user.id,
+        name: user.name,
+        email: user.email, 
+      },
     });
   } catch (error) {
     res.status(500).json({
